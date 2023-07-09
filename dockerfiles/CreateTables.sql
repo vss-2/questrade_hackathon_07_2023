58ef5128572a6aaca6e6c9ebab1d35d549ced7f0
Vitor Sousa <32624576+vss-2@users.noreply.github.com>
2023-07-09 01:24:59 -0300

    fix: correct wrong sql statement order and relationships

--- dockerfiles/CreateTables.sql
+++ dockerfiles/CreateTables.sql
@@ -1,16 +1,53 @@
 CREATE TABLE ACCOUNTS (
-    INT PRIMARY KEY SERIAL AccountID,
-    VARCHAR(64) Email,
-    VARCHAR(64) Password,
-    INT AccountType,
-    DATE DEFAULT NOW() CreateAt
+    AccountID INT GENERATED ALWAYS AS IDENTITY,
+    Email VARCHAR(64),
+    Password VARCHAR(64),
+    AccountType INT,
+    CreateAt DATE DEFAULT NOW(),
+    PRIMARY KEY(AccountID)
 );
 
 CREATE TABLE CLIENTS (
-    INT AccountID REFERENCES ACCOUNTS.AccountID,
-    VARCHAR(16) SocialSecurityNumber,
-    VARCHAR(16) Telephone,
-    DATE DEFAULT NULL LastScoreCheck,
-    INT DEFAULT NULL Score,
-    VARCHAR(32) DocUUID
-)
+    AccountID INT,
+    SocialSecurityNumber VARCHAR(16),
+    Telephone VARCHAR(16),
+    LastScoreCheck DATE DEFAULT NULL,
+    Score INT DEFAULT NULL,
+    DocUUID VARCHAR(32),
+    CONSTRAINT fk_AccountID 
+      FOREIGN KEY(AccountID) 
+        REFERENCES ACCOUNTS(AccountID)
+);
+
+CREATE TABLE CONTRACTS (
+    ContractID INT GENERATED ALWAYS AS IDENTITY,
+    AccountID INT,
+    Loan INT,
+    InterestRate INT,
+    Currency VARCHAR(8),
+    Quotes INT,
+    CONSTRAINT fk_AccountID 
+        FOREIGN KEY(AccountID) 
+            REFERENCES ACCOUNTS(AccountID),
+    PRIMARY KEY(ContractID)
+);
+
+CREATE TABLE CONTRACT_DATE (
+    ContractID INT,
+    TimeRange INT,
+    TimeUnity VARCHAR(8),
+    StartDate DATE,
+    EndDate DATE,
+    CONSTRAINT fk_ContractID 
+        FOREIGN KEY(ContractID) 
+            REFERENCES CONTRACTS(ContractID)
+);
+
+CREATE TABLE PAYMENTS (
+    ContractID INT, 
+    Quote INT,
+    Doc_UUID_Receipt VARCHAR(64) DEFAULT NULL,
+    CONSTRAINT fk_ContractID 
+        FOREIGN KEY(ContractID)
+            REFERENCES CONTRACTS(ContractID)
+);
